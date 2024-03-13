1069dd6f4ccee44ab26b5536becd81a381994dad
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-03-13 19:01:47 +0100

    importato font, aggiunto immagine, impostato la prima pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    colors: {
+      Nutmeg: "hsl(14, 45%, 36%)",
+      DarkRaspberry: "hsl(332, 51%, 32%)",
+      white: "hsl(0, 0%, 100%)",
+      RoseWhite: "hsl(330, 100%, 98%)",
+      Eggshell: "hsl(30, 54%, 90%)",
+      LightGray: "hsl(30, 18%, 87%)",
+      WengeBrown: "hsl(30, 10%, 34%)",
+      DarkCharcoal: "hsl(24, 5%, 18%)",
+    },
+    fontFamily: {
+      young: ["Young Serif", "serif"],
+      outfit: ["Outfit", "sans-serif"],
+    },
     extend: {},
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
